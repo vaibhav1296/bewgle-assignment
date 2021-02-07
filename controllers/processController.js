9774d3ca73ea3e0db32ec8ee692458750f7e5ce2
Vaibhav Sharma <sharmavaibhav5796@gmail.com>
2021-02-08 00:43:02 +0530

    endpoint starts with /process

---
+++ controllers/processController.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const utility = require("../services/utilityService");
+const { responseTimeConfig } = require("../config/customConfig");
+const middleware = require("../services/middlewares");
+const query = require("../services/query/query");
+
+router.get("/get-data", middleware.extractInfoFromReq, async (req, res) => {
+  try {
+    res.locals.reqData.method = "GET";
+    const reply = await query.insertRequestData({ ...res.locals.reqData });
+    if (utility.isNullOrUndefined(reply)) {
+      res.status(400).send({
+        message: "SOMETHING WENT WRONG WHILE INSERTION",
+      });
+    } else {
+      setTimeout(() => {
+        res.status(200).send({
+          message: "Data is successfully inserted.",
+          body: res.locals.reqData,
+        });
+      }, res.locals.reqData.duration * 1000);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+router.post("/post-data", middleware.extractInfoFromReq, async (req, res) => {
+  try {
+    res.locals.reqData.method = "POST";
+    const reply = await query.insertRequestData({ ...res.locals.reqData });
+    if (utility.isNullOrUndefined(reply)) {
+      res.status(400).send({
+        message: "SOMETHING WENT WRONG WHILE INSERTION",
+      });
+    } else {
+      setTimeout(() => {
+        res.status(200).send({
+          message: "Data is successfully inserted.",
+          body: res.locals.reqData,
+        });
+      }, res.locals.reqData.duration * 1000);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+router.put("/update-data", middleware.extractInfoFromReq, async (req, res) => {
+  try {
+    res.locals.reqData.method = "UPDATE";
+    const reply = await query.insertRequestData({ ...res.locals.reqData });
+    if (utility.isNullOrUndefined(reply)) {
+      res.status(400).send({
+        message: "SOMETHING WENT WRONG WHILE INSERTION",
+      });
+    } else {
+      setTimeout(() => {
+        res.status(200).send({
+          message: "Data is successfully inserted.",
+          body: res.locals.reqData,
+        });
+      }, res.locals.reqData.duration * 1000);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+router.delete(
+  "/update-data",
+  middleware.extractInfoFromReq,
+  async (req, res) => {
+    try {
+      res.locals.reqData.method = "DELETE";
+      const reply = await query.insertRequestData({ ...res.locals.reqData });
+      if (utility.isNullOrUndefined(reply)) {
+        res.status(400).send({
+          message: "SOMETHING WENT WRONG WHILE INSERTION",
+        });
+      } else {
+        setTimeout(() => {
+          res.status(200).send({
+            message: "Data is successfully inserted.",
+            body: res.locals.reqData,
+          });
+        }, res.locals.reqData.duration * 1000);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+  }
+);
+
+module.exports = router;
