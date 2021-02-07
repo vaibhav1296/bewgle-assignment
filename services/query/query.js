daa036093dbef5237f084df9e598d8913e641436
Vaibhav Sharma <sharmavaibhav5796@gmail.com>
2021-02-08 00:45:33 +0530

    all db queries

---
+++ services/query/query.js
@@ -0,0 +1,100 @@
+const models = require("../../models/index");
+const sequelizeOperator = require("sequelize").Op;
+const sequelize = require("sequelize");
+
+const insertRequestData = async ({
+  method,
+  path,
+  query,
+  body,
+  duration,
+  headers,
+}) => {
+  try {
+    return models.request.create({
+      method,
+      headers,
+      path,
+      query,
+      duration,
+      body,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const findAndCountReq = async (method) => {
+  try {
+    return models.request.findAndCountAll({
+      where: { method },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getRequestByFromDate = async (method, fromDate) => {
+  return models.request.findAndCountAll({
+    where: {
+      method,
+      createdAt: {
+        [sequelizeOperator.gte]: fromDate,
+      },
+    },
+  });
+};
+const getRequestByToDate = async (method, toDate) => {
+  return models.request.findAndCountAll({
+    where: {
+      method,
+      createdAt: {
+        [sequelizeOperator.lte]: toDate,
+      },
+    },
+  });
+};
+
+const getCountOfReqByMethod = async (method) => {
+  return models.request.count({
+    where: {
+      method,
+    },
+  });
+};
+
+const getSumOfDuration = async (method) => {
+  return models.request.findOne({
+    where: {
+      method,
+    },
+    attributes: [
+      [
+        models.sequelize.fn("sum", models.sequelize.col("duration")),
+        "duration",
+      ],
+    ],
+  });
+};
+
+const getRequstByFromAndToDate = async (method, fromDate, toDate) => {
+  return models.request.findAndCountAll({
+    where: {
+      method,
+      [models.Sequelize.Op.and]: [
+        { createdAt: { [models.Sequelize.Op.gte]: fromDate } },
+        { createdAt: { [models.Sequelize.Op.lte]: toDate } },
+      ],
+    },
+  });
+};
+
+module.exports = {
+  insertRequestData,
+  findAndCountReq,
+  getSumOfDuration,
+  getRequstByFromAndToDate,
+  getRequestByFromDate,
+  getCountOfReqByMethod,
+  getRequestByToDate,
+};
